7b67e3b55058a888d9004fd9dd7c903328ac2368
kanda0227 <k1311067@gmail.com>
2018-11-15 08:59:03 +0900

    Green：カードインスタンスを作成

---
+++ PokerByTDD/Model/Card.swift
@@ -0,0 +1,56 @@
+//
+//  Card.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/11/15.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import Foundation
+
+/// トランプのカード
+struct Card {
+    
+    let suit: Suit
+    let rank: Rank
+}
+
+enum Suit {
+    /// ハート
+    case heart
+    /// スペード
+    case spade
+    /// クラブ
+    case club
+    /// ダイヤ
+    case diamond
+}
+
+enum Rank {
+    /// エース
+    case ace
+    /// 2
+    case two
+    /// 3
+    case three
+    /// 4
+    case four
+    /// 5
+    case five
+    /// 6
+    case six
+    /// 7
+    case seven
+    /// 8
+    case eight
+    /// 9
+    case nine
+    /// 10
+    case ten
+    /// ジャック
+    case jack
+    /// クイーン
+    case queen
+    /// キング
+    case king
+}
