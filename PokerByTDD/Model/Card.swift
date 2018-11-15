88002de7602639a35e38748def720321d9185091
kanda0227 <k1311067@gmail.com>
2018-11-15 09:11:01 +0900

    リファクタリング：enum を Card 構造体の中にしまう

--- PokerByTDD/Model/Card.swift
+++ PokerByTDD/Model/Card.swift
@@ -11,46 +11,46 @@ import Foundation
 /// トランプのカード
 struct Card {
     
+    enum Suit {
+        /// ハート
+        case heart
+        /// スペード
+        case spade
+        /// クラブ
+        case club
+        /// ダイヤ
+        case diamond
+    }
+    
+    enum Rank {
+        /// エース
+        case ace
+        /// 2
+        case two
+        /// 3
+        case three
+        /// 4
+        case four
+        /// 5
+        case five
+        /// 6
+        case six
+        /// 7
+        case seven
+        /// 8
+        case eight
+        /// 9
+        case nine
+        /// 10
+        case ten
+        /// ジャック
+        case jack
+        /// クイーン
+        case queen
+        /// キング
+        case king
+    }
+    
     let suit: Suit
     let rank: Rank
-}
-
-enum Suit {
-    /// ハート
-    case heart
-    /// スペード
-    case spade
-    /// クラブ
-    case club
-    /// ダイヤ
-    case diamond
-}
-
-enum Rank {
-    /// エース
-    case ace
-    /// 2
-    case two
-    /// 3
-    case three
-    /// 4
-    case four
-    /// 5
-    case five
-    /// 6
-    case six
-    /// 7
-    case seven
-    /// 8
-    case eight
-    /// 9
-    case nine
-    /// 10
-    case ten
-    /// ジャック
-    case jack
-    /// クイーン
-    case queen
-    /// キング
-    case king
 }
