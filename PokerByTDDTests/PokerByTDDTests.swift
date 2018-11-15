afa61ad0f38e210adc57f702de2306dc6944ae04
kanda0227 <k1311067@gmail.com>
2018-11-16 08:36:19 +0900

    Green(リファクタ)：一般化についてテストを増量

--- PokerByTDDTests/PokerByTDDTests.swift
+++ PokerByTDDTests/PokerByTDDTests.swift
@@ -44,12 +44,25 @@ class PokerByTDDTests: XCTestCase {
     
     func testCardEqual() {
         
+        // rank: 等しい suit: 等しい
         let card1 = Card(rank: .three, suit: .heart)
         let card2 = Card(rank: .three, suit: .heart)
         XCTAssertEqual(card1, card2)
         
+        // rank: 等しくない suit: 等しくない
         let card3 = Card(rank: .three, suit: .heart)
         let card4 = Card(rank: .ace, suit: .club)
         XCTAssertNotEqual(card3, card4)
+        
+        // rank: 等しい suit: 等しくない
+        let card5 = Card(rank: .three, suit: .heart)
+        let card6 = Card(rank: .three, suit: .club)
+        XCTAssertNotEqual(card5, card6)
+
+        // rank: 等しくない suit: 等しい
+        let card7 = Card(rank: .three, suit: .heart)
+        let card8 = Card(rank: .ace, suit: .heart)
+        XCTAssertNotEqual(card7, card8)
+
     }
 }
