ed4e04a0f46b25d79dac4c5d024e3bc23e9c2ce9
kanda0227 <k1311067@gmail.com>
2018-11-15 09:32:46 +0900

    Green(リファクタ)：不要なテストを削除
    testCardNotation だけで testInitializeCard のテストが通ることがわかるため，冗長なテストになっている

--- PokerByTDDTests/PokerByTDDTests.swift
+++ PokerByTDDTests/PokerByTDDTests.swift
@@ -10,17 +10,6 @@ import XCTest
 @testable import PokerByTDD
 
 class PokerByTDDTests: XCTestCase {
-
-    func testInitializeCard() {
-        
-        let card1 = Card(rank: .three, suit: .heart)
-        XCTAssertEqual(card1.suit, .heart)
-        XCTAssertEqual(card1.rank, .three)
-        
-        let card2 = Card(rank: .king, suit: .diamond)
-        XCTAssertEqual(card2.suit, .diamond)
-        XCTAssertEqual(card2.rank, .king)
-    }
     
     func testCardNotation() {
         
