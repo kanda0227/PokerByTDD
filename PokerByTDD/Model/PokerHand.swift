1706be15df69b313969042d7c0d6c63846fe4a93
kanda0227 <k1311067@gmail.com>
2018-11-17 16:40:08 +0900

    Green：PokerHand をファイル移動

---
+++ PokerByTDD/Model/PokerHand.swift
@@ -0,0 +1,45 @@
+//
+//  PokerHand.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/11/17.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import Foundation
+
+/// ポーカーの役一覧
+enum PokerHand: CaseIterable {
+    case pair
+    case flash
+    case highCard
+    
+    func isPockerHand(cards: [Card]) -> Bool {
+        switch self {
+        case .pair:
+            return isPair(cards)
+        case .flash:
+            return isFlash(cards)
+        case .highCard:
+            // ハイカード以外の，どの役にも当てはまらない場合に true を返します
+            return PokerHand.allCases
+                .filter { $0 != .highCard }
+                .filter { $0.isPockerHand(cards: cards) }
+                .isEmpty
+        }
+    }
+    
+    private func isPair(_ cards: [Card]) -> Bool {
+        let maxSameSuitCount = cards.enumerated().map { index, card in
+            cards.filter { card.hasSameRank($0) }.count
+            }.max()
+        return maxSameSuitCount == 2
+    }
+    
+    private func isFlash(_ cards: [Card]) -> Bool {
+        // カードが1枚もないときはフラッシュではないとする
+        guard let card = cards.first else { return false }
+        // 1枚だと常にフラッシュ！
+        return cards.filter { card.hasSameSuit($0) }.count == cards.count
+    }
+}
