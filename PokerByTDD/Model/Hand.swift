5b4d917f315fe0cad3984ec441c36a351b06187f
kanda0227 <k1311067@gmail.com>
2018-11-17 09:40:14 +0900

    Green(リファクタ)：役の追加を考慮して役一覧を enum に設定・ハイカードが「他の役に当てはまらない場合」であることを保証

--- PokerByTDD/Model/Hand.swift
+++ PokerByTDD/Model/Hand.swift
@@ -17,21 +17,50 @@ struct Hand {
     }
     
     var isPair: Bool {
+        return PokerHand.pair.isPockerHand(cards: cards)
+    }
+    
+    var isFlash: Bool {
+        return PokerHand.flash.isPockerHand(cards: cards)
+    }
+    
+    var isHighCard: Bool {
+        return PokerHand.highCard.isPockerHand(cards: cards)
+    }
+}
+
+/// ポーカーの役一覧
+private enum PokerHand: CaseIterable {
+    case pair
+    case flash
+    case highCard
+    
+    func isPockerHand(cards: [Card]) -> Bool {
+        switch self {
+        case .pair:
+            return isPair(cards)
+        case .flash:
+            return isFlash(cards)
+        case .highCard:
+            // ハイカード以外の，どの役にも当てはまらない場合に true を返します
+            return PokerHand.allCases
+                .filter { $0 != .highCard }
+                .filter { $0.isPockerHand(cards: cards) }
+                .isEmpty
+        }
+    }
+    
+    private func isPair(_ cards: [Card]) -> Bool {
         let maxSameSuitCount = cards.enumerated().map { index, card in
             cards.filter { card.hasSameRank($0) }.count
             }.max()
         return maxSameSuitCount == 2
     }
     
-    var isFlash: Bool {
+    private func isFlash(_ cards: [Card]) -> Bool {
         // カードが1枚もないときはフラッシュではないとする
         guard let card = cards.first else { return false }
         // 1枚だと常にフラッシュ！
         return cards.filter { card.hasSameSuit($0) }.count == cards.count
-    }
-    
-    var isHighCard: Bool {
-        // この実装はペアとフラッシュしかない前提になっているのでよくない
-        return !(isPair || isFlash)
     }
 }
