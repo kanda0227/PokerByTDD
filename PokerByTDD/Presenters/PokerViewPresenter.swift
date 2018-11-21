3b5b689cdc3d071a61a0e96f2c29b76fdbe5a53e
kanda0227 <k1311067@gmail.com>
2018-11-22 08:50:43 +0900

    UI：Presenter を作成し，VCと接続
    ロジックをいじる箇所は Presenter へ移行
    VCはビューに専念してもらう

---
+++ PokerByTDD/Presenters/PokerViewPresenter.swift
@@ -0,0 +1,33 @@
+//
+//  PokerViewPresenter.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/11/22.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class PokerViewPresenter {
+    
+    private let dealer = Dealer()
+    private let useCardNum = 5
+    
+    private let updateCards: Binder<[Card]>
+    private let handText: Binder<String>
+    
+    init(updateCards: Binder<[Card]>,
+         handText: Binder<String>) {
+        self.updateCards = updateCards
+        self.handText = handText
+    }
+    
+    func postStart(gatherCards: [Card]) {
+        dealer.gatherCards(gatherCards)
+        let cards = dealer.dealCards(useCardNum)
+        updateCards.onNext(cards)
+        handText.onNext(Hand(cards: cards).hand().text)
+    }
+}
