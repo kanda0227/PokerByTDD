0a2c734047e14777c5efc3ace63294b30326b431
kanda0227 <k1311067@gmail.com>
2018-12-02 18:46:46 +0900

    UI：カードデザインの設定画面を作成 (空実装)

---
+++ PokerByTDD/ViewControllers/CardDesignViewController.swift
@@ -0,0 +1,11 @@
+//
+//  CardDesignViewController.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/12/02.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import UIKit
+
+final class CardDesignViewController: UIViewController {}
