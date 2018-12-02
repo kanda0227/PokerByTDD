74d1e7b6e7433bccae2d42d6856d278db361190a
kanda0227 <k1311067@gmail.com>
2018-12-02 20:54:57 +0900

    UI：画像の選択処理を追加
    リサイズとかできるようにしたい
    決定ボタンの動作が未実装

--- PokerByTDD/ViewControllers/CardDesignChoiceViewController.swift
+++ PokerByTDD/ViewControllers/CardDesignChoiceViewController.swift
@@ -8,11 +8,46 @@
 
 import UIKit
 
-final class CardDesignChoiceViewController: UIViewController {
+final class CardDesignChoiceViewController: UIViewController, UINavigationControllerDelegate {
+    
+    @IBOutlet private weak var pickedImage: UIImageView!
     
     static func instantiate(category: CardDesignCategory) -> CardDesignChoiceViewController {
         let vc = UIViewController.instantiate(withStoryboardID: "CardDesignChoiceView") as! CardDesignChoiceViewController
         vc.title = category.rawValue
         return vc
+    }
+    
+    @IBAction func tapSelectFromAlbumButton(_ sender: Any) {
+        presentImagePickerVC(type: .photoLibrary)
+    }
+    
+    @IBAction func tapTakePictureButton(_ sender: Any) {
+        presentImagePickerVC(type: .camera)
+    }
+    
+    @IBAction func tapDoneButton(_ sender: Any) {
+        
+    }
+    
+    private func presentImagePickerVC(type: UIImagePickerController.SourceType) {
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = type
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true, completion: nil)
+    }
+}
+
+extension CardDesignChoiceViewController: UIImagePickerControllerDelegate {
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            pickedImage.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
