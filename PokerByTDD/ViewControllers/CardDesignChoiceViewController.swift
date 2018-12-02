317ee14b9115eca3b3b2f75a6a0cc69c626f9477
kanda0227 <k1311067@gmail.com>
2018-12-02 19:48:16 +0900

    UI：カードの画像を選択する画面を作成

---
+++ PokerByTDD/ViewControllers/CardDesignChoiceViewController.swift
@@ -0,0 +1,11 @@
+//
+//  CardDesignChoiceViewController.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/12/02.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import UIKit
+
+final class CardDesignChoiceViewController: UIViewController {}
