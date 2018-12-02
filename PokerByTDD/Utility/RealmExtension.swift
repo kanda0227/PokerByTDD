d543a635d8762c99c509780e0c5e1f9c7b47df6d
kanda0227 <k1311067@gmail.com>
2018-12-02 22:30:26 +0900

    UI：Realm で UIImage を扱うためのメソッドを追加

---
+++ PokerByTDD/Utility/RealmExtension.swift
@@ -0,0 +1,46 @@
+//
+//  RealmExtension.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/12/02.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import RealmSwift
+import Realm
+import UIKit
+
+class ImageObject: Object {
+    @objc dynamic var data: Data = Data()
+    @objc dynamic var key: String = ""
+    
+    /// 絶対呼んでね！
+    func set(data: Data, key: String) {
+        self.data = data
+        self.key = key
+    }
+}
+
+extension Realm {
+    
+    func saveImage(_ image: UIImage, key: String) {
+        if let data = image.pngData() {
+            if let imageObj = objects(ImageObject.self).filter("key = '\(key)'").first {
+                try! write {
+                    imageObj.data = data
+                }
+            } else {
+                let imageObj = ImageObject()
+                imageObj.set(data: data, key: key)
+                try! write {
+                    add(imageObj)
+                }
+            }
+        }
+    }
+    
+    func restoreImage(key: String) -> UIImage? {
+        guard let imageObj = objects(ImageObject.self).filter("key = '\(key)'").first else { return nil }
+        return UIImage(data: imageObj.data)
+    }
+}
