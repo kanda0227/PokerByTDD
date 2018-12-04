e68029ab94949269909a397626ddf99c6f241c14
kanda0227 <k1311067@gmail.com>
2018-12-05 07:28:38 +0900

    モジュール分割：Utility モジュールの作成

---
+++ Utility/Utility.h
@@ -0,0 +1,19 @@
+//
+//  Utility.h
+//  Utility
+//
+//  Created by Kanda Sena on 2018/12/05.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Utility.
+FOUNDATION_EXPORT double UtilityVersionNumber;
+
+//! Project version string for Utility.
+FOUNDATION_EXPORT const unsigned char UtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utility/PublicHeader.h>
+
+
