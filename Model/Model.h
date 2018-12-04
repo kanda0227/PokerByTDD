45d8bcd50683b36aedfba0571a42600fae35fc77
kanda0227 <k1311067@gmail.com>
2018-12-04 09:16:13 +0900

    モジュール分割：Model フォルダ内をモジュール化

---
+++ Model/Model.h
@@ -0,0 +1,19 @@
+//
+//  Model.h
+//  Model
+//
+//  Created by Kanda Sena on 2018/12/04.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Model.
+FOUNDATION_EXPORT double ModelVersionNumber;
+
+//! Project version string for Model.
+FOUNDATION_EXPORT const unsigned char ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Model/PublicHeader.h>
+
+
