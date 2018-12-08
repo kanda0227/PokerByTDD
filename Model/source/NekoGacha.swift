75d3c60e493344ae902c1714c209a1982453180b
kanda0227 <k1311067@gmail.com>
2018-12-08 20:32:37 +0900

    Model：ねこガチャシステム追加

---
+++ Model/source/NekoGacha.swift
@@ -0,0 +1,51 @@
+//
+//  NekoGacha.swift
+//  Model
+//
+//  Created by Kanda Sena on 2018/12/08.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import Foundation
+
+public final class NekoGacha {
+    
+    private init() {}
+    
+    public let shared = NekoGacha()
+    
+    public func get() -> Neko {
+        let neko = Neko.allCases.randomElement()!
+        save(neko: neko)
+        return neko
+    }
+    
+    private func save(neko: Neko) {
+        UserDefaults.standard.set(true, forKey: neko.hasNekoKey())
+    }
+    
+    private func restore(neko: Neko) -> Bool {
+        return UserDefaults.standard.object(forKey: neko.hasNekoKey()) as? Bool ?? false
+    }
+}
+
+public enum Neko: String, CaseIterable {
+    /// みけねこ
+    case mike
+    /// くろねこ
+    case black
+    /// しろねこ
+    case white
+    /// ロシアンブルー
+    case russianBlue
+    /// くつした
+    case socks
+    
+    private var hasKey: String {
+        return "hasNekoKey"
+    }
+    
+    fileprivate func hasNekoKey() -> String {
+        return hasKey + rawValue
+    }
+}
