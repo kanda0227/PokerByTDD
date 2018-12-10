c325edd72b6dbde0fa2c1c7bd57a41e9426a9f5e
kanda0227 <k1311067@gmail.com>
2018-12-11 07:54:22 +0900

    UI (リファクタ)：所持ねこ画面の Presenter にデータを保持，必要な分だけ公開させる

--- Presenter/source/NekoLibraryPresenter.swift
+++ Presenter/source/NekoLibraryPresenter.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import Model
+
+public final class NekoLibraryPresenter {
+    
+    public init() {}
+    
+    private let nekos = Neko.allCases
+    
+    public func numberOfSections() -> Int {
+        return 1
+    }
+    
+    public func numberOfItemsInSection(_ section: Int) -> Int {
+        return nekos.count
+    }
+    
+    public func neko(at indexPath: IndexPath) -> Neko {
+        return nekos[indexPath.item]
+    }
+}
