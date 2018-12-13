c2118aa5d3ee81d7457307dd475e88a9eb061857
kanda0227 <k1311067@gmail.com>
2018-12-13 22:16:17 +0900

    所持金回復カウンター：所持金回復カウンターをビューに追加

--- Design/source/WalletView.swift
+++ Design/source/WalletView.swift
@@ -12,6 +12,7 @@ import UIKit
     
     @IBOutlet private weak var walletLabel: UILabel!
     @IBOutlet private weak var itemNameLabel: UILabel!
+    @IBOutlet private weak var recoveryLabel: UILabel!
     
     @IBInspectable public var money: Int = 10000 {
         didSet {
@@ -43,8 +44,22 @@ import UIKit
     }
     
     override public var intrinsicContentSize: CGSize {
-        let walletLabelSize = walletLabel.bounds.size
-        let itemNameLabelSize = itemNameLabel.bounds.size
-        return CGSize(width: walletLabelSize.width + itemNameLabelSize.width, height: walletLabelSize.height)
+        let walletLabelSize = walletLabel.intrinsicContentSize
+        let itemNameLabelSize = itemNameLabel.intrinsicContentSize
+        let recoveryLabelSize = recoveryLabel.intrinsicContentSize
+        return CGSize(width: walletLabelSize.width + itemNameLabelSize.width + recoveryLabelSize.width, height: walletLabelSize.height)
+    }
+    
+    public func set(second: Int, presentMoneyPerTime: Int, shouldPresentMoney: Bool) {
+        recoveryLabel.text = recoveryText(second: second, presentMoneyPerTime: presentMoneyPerTime, shouldPresentMoney: shouldPresentMoney)
+        invalidateIntrinsicContentSize()
+    }
+    
+    private func recoveryText(second: Int, presentMoneyPerTime: Int, shouldPresentMoney: Bool) -> String {
+        if shouldPresentMoney {
+            return "あと\(second)秒で\(presentMoneyPerTime)円あげるにゃ　"
+        } else {
+            return "お金が無くなってきたらまたあげるにゃ　"
+        }
     }
 }
