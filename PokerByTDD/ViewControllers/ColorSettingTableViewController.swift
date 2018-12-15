a4377a05ea84ee145a138f3ba381e7c6b7bb439f
kanda0227 <k1311067@gmail.com>
2018-12-15 23:30:54 +0900

    UI：カラーテーマ設定画面作成
    セルに色が反映されない不具合があり...

---
+++ PokerByTDD/ViewControllers/ColorSettingTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ColorSettingTableViewController.swift
+//  PokerByTDD
+//
+//  Created by Kanda Sena on 2018/12/15.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import UIKit
+import Utility
+import Design
+import Presenter
+
+final class ColorSettingTableViewController: UITableViewController {
+    
+    private lazy var presenter = ColorSettingPresenter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.register(ColorSettingCell.self)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return presenter.numberOfSections()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: ColorSettingCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.set(presenter.item(at: indexPath))
+        return cell
+    }
+}
