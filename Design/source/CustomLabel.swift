8f74d01a61e61e357104d92815de6f06f08088ec
kanda0227 <k1311067@gmail.com>
2018-12-20 22:19:08 +0900

    UI：ColorSet に対応した CustomLabel クラスを作成

---
+++ Design/source/CustomLabel.swift
@@ -0,0 +1,13 @@
+//
+//  CustomLabel.swift
+//  Design
+//
+//  Created by Kanda Sena on 2018/12/20.
+//  Copyright © 2018 sena.kanda. All rights reserved.
+//
+
+import UIKit
+import Model
+import Utility
+
+public class CustomLabel: UILabel, ColorSetViewProtocol {}
