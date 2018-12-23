15d6194fca51af6b140c34cb2acdc9dc4860c15a
kanda0227 <k1311067@gmail.com>
2018-12-23 20:24:38 +0900

    UI：ColorSet の色がセルに反映されない問題の対策

--- Design/source/ColorSettingCell.swift
+++ Design/source/ColorSettingCell.swift
@@ -17,12 +17,31 @@ public final class ColorSettingCell: UITableViewCell {
     @IBOutlet private weak var tabBarItemColorView: UIView!
     @IBOutlet private weak var textSampleLabel: UILabel!
     
+    private var colorSet: ColorSet? {
+        didSet {
+            setupColor()
+        }
+    }
+    
     public func set(_ colorSet: ColorSet) {
+        self.colorSet = colorSet
+    }
+    
+    private func setupColor() {
+        guard let colorSet = colorSet else { return }
         navigationBarColorView.backgroundColor = colorSet.navigationBarColor()
         backgroundColorView.backgroundColor = colorSet.backgroundColor()
         tabBarColorView.backgroundColor = colorSet.tabBarColor()
         tabBarItemColorView.backgroundColor = colorSet.tabBarItemColor()
         textSampleLabel.textColor = colorSet.textColor()
         textSampleLabel.text = colorSet.rawValue
+        self.layer.borderWidth = isSelected ? 2 : 0
+        contentView.layer.borderColor = colorSet.navigationBarColor().cgColor
+    }
+    
+    override public var isSelected: Bool {
+        didSet {
+            setupColor()
+        }
     }
 }
