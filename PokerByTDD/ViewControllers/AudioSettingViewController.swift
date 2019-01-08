8bb5638a485a9a811d9a3915e39336fa4a963982
kanda0227 <k1311067@gmail.com>
2019-01-08 23:55:36 +0900

    UI：ピッカーで音楽を選択する処理を追加
    細かいところの設定は後日

--- PokerByTDD/ViewControllers/AudioSettingViewController.swift
+++ PokerByTDD/ViewControllers/AudioSettingViewController.swift
@@ -17,6 +17,13 @@ final class AudioSettingViewController: UIViewController, ColorSetViewProtocol {
     @IBOutlet private weak var musicSwitch: UISwitch!
     @IBOutlet private weak var musicSlider: UISlider!
     @IBOutlet private weak var musicSelectTextField: UITextField!
+    private var picker: UIPickerView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupPickerView()
+    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -49,5 +56,48 @@ final class AudioSettingViewController: UIViewController, ColorSetViewProtocol {
         musicSwitch.isOn = AudioHelper.shared.isOnMusicSwitch()
         musicSlider.value = AudioHelper.shared.musicVolumeValue()
         musicSelectTextField.text = AudioHelper.shared.currentMusic().musicName()
+    }
+    
+    private func setupPickerView() {
+        picker = UIPickerView()
+        picker?.delegate = self
+        picker?.dataSource = self
+        picker?.showsSelectionIndicator = true
+        
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 0, height: 35))
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(AudioSettingViewController.doneSelectMusic))
+        let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(AudioSettingViewController.cancelSelectMusic))
+        toolbar.setItems([cancelButton, doneButton], animated: true)
+        
+        self.musicSelectTextField.inputView = picker
+        self.musicSelectTextField.inputAccessoryView = toolbar
+    }
+    
+    @objc private func doneSelectMusic() {
+        let selectedMusic = (picker?.selectedRow(inComponent: 0)).flatMap { MusicAudio.allCases[$0] }
+        musicSelectTextField.text = selectedMusic?.musicName()
+        selectedMusic.map(AudioHelper.shared.setMusic)
+        musicSelectTextField.endEditing(true)
+    }
+    
+    @objc private func cancelSelectMusic() {
+        musicSelectTextField.endEditing(true)
+    }
+}
+
+extension AudioSettingViewController: UIPickerViewDelegate {}
+
+extension AudioSettingViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return MusicAudio.allCases.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return MusicAudio.allCases[row].musicName()
     }
 }
