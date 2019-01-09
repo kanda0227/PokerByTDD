9817f5a7c053683229557b46a99e3d6f22f358c4
kanda0227 <k1311067@gmail.com>
2019-01-09 23:37:12 +0900

    UI：音楽選択ピッカーの内容表示制御を Presenter へ移動

---
+++ Presenter/source/AudioSettingPresenter.swift
@@ -0,0 +1,29 @@
+//
+//  AudioSettingPresenter.swift
+//  Presenter
+//
+//  Created by Kanda Sena on 2019/01/09.
+//  Copyright © 2019 sena.kanda. All rights reserved.
+//
+
+import Foundation
+import Model
+
+public final class AudioSettingPresenter {
+    
+    private let items = MusicAudio.allCases
+    
+    public init() {}
+    
+    public func numberOfComponents() -> Int {
+        return 1
+    }
+    
+    public func numberOfRowsInComponent(_ component: Int) -> Int {
+        return items.count
+    }
+    
+    public func title(row: Int, component: Int) -> String? {
+        return items[row].musicName()
+    }
+}
