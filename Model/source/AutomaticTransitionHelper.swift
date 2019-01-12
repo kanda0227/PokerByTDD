2ceeb48bfc9f46c110cca938dc671da90ec657b6
kanda0227 <k1311067@gmail.com>
2019-01-12 23:57:13 +0900

    Model：自動遷移にタブ切り替えだけでなくモーダル画面も追加

--- Model/source/AutomaticTransitionHelper.swift
+++ Model/source/AutomaticTransitionHelper.swift
@@ -13,15 +13,15 @@ public final class AutomaticTransitionHelper {
     
     static public let shared = AutomaticTransitionHelper()
     
-    private let subject = PublishSubject<TopTab>()
+    private let subject = PublishSubject<Screen>()
     
     private init() {}
     
-    public func post(_ tab: TopTab) {
+    public func post(_ tab: Screen) {
         subject.onNext(tab)
     }
     
-    public func observable() -> Observable<TopTab> {
+    public func observable() -> Observable<Screen> {
         return subject.asObservable()
     }
 }
@@ -32,4 +32,37 @@ public enum TopTab: Int {
     case nekoRoom
     case shop
     case setting
+}
+
+public enum Screen {
+    case poker
+    case nekoGacha
+    case nekoRoom
+    case shop
+    case setting
+    case nekoLibrary
+    
+    public var tab: TopTab {
+        switch self {
+        case .poker:
+            return .poker
+        case .nekoGacha, .nekoLibrary:
+            return .nekoGacha
+        case .nekoRoom:
+            return .nekoRoom
+        case .shop:
+            return .shop
+        case .setting:
+            return .setting
+        }
+    }
+    
+    public var presentVC: Screen? {
+        switch self {
+        case .nekoLibrary:
+            return self
+        default:
+            return nil
+        }
+    }
 }
